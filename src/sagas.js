9076eb8693c5037568ad5dfe3cbbad8b7ec3124c
DESKTOP-B1OEVKN\marcogomesr <marcogomesr@gmail.com>
2023-09-14 17:40:22 +0200

    add saga generators func

---
+++ src/sagas.js
@@ -0,0 +1,18 @@
+import { put, takeEvery } from 'redux-saga/effects'
+import { ADD_TODO, ADD_TODO_ASYNC, REMOVE_TODO } from './types'
+
+function* addTodoAsync (action) {
+  yield put({ type: ADD_TODO, payload: action.payload})
+}
+
+function* removeTodoAsync (action) {
+  yield put({type: REMOVE_TODO, payload: action.payload})
+}
+
+export function* watchAddTodo () {
+  yield takeEvery(ADD_TODO_ASYNC, addTodoAsync)
+}
+
+export function* watchRemoveTodo() {
+  yield takeEvery(REMOVE_TODO_ASYNC, removeTodoAsync)
+}
