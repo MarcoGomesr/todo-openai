a7e77cb712778c63917eb424f65ff6555752348b
DESKTOP-B1OEVKN\marcogomesr <marcogomesr@gmail.com>
2023-09-14 17:59:08 +0200

    move login from Todoapp to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import TodoApp from './TodoApp'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_TODO_ASYNC, REMOVE_TODO_ASYNC } from './types';
 
-function App() {
 
-  return (
-    <>
-    <TodoApp />      
-    </>
-  )
+export default function App() {
+  const dispatch = useDispatch()
+  const todos = useSelector( (state) => state.todos)
+  const [newTodo, setNewTodo] = useState('')
+  
+
+
+const handleAddTodo = () =>{
+  if ( newTodo.trim() === "") return
+  dispatch({type: ADD_TODO_ASYNC, payload: newTodo})
+  setNewTodo('')
 }
 
-export default App
+const handleRemoveTodo = ( index) => {
+  dispatch({type: REMOVE_TODO_ASYNC, payload: index})
+}
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" placeholder='New Todo' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+      <button onClick={handleAddTodo}>Add</button>
+
+      <ul>
+        {todos.map( (todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)} >Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
