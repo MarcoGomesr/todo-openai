03a8020d0d3d555047ce68850855cee3e60da298
DESKTOP-B1OEVKN\marcogomesr <marcogomesr@gmail.com>
2023-09-15 11:30:32 +0200

    add redux saga trigger

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,11 @@
-import { ADD_TODO, REMOVE_TODO } from "./types";
+import { ADD_TODO_ASYNC, REMOVE_TODO_ASYNC } from "./types";
 
 export const addTodo = (text) =>({
-  type: ADD_TODO,
+  type: ADD_TODO_ASYNC,
   payload: text
 })
 
 export const removeTodo = (index) => ({
-  type: REMOVE_TODO,
+  type: REMOVE_TODO_ASYNC,
   payload: index
 })
